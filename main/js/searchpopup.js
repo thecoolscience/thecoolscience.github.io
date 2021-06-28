e73a54b0f2e36d5b2448f53640394f732fd0f61e
Kali Live user <thecoolscience1820@gmail.com>
2021-06-28 22:59:47 +0000

    webpage contents

---
+++ main/js/searchpopup.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function searchpopupfn() {
+   document.getElementById("popup1").classList.toggle("showpopup")
+}
