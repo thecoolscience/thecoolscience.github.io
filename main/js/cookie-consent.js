9b814c7463a3baed71b31555f6708e8796fc3b5e
Kali Live user <thecoolscience1820@gmail.com>
2021-07-04 00:40:16 +0000

    webpage contents

--- main/js/cookie-consent.js
+++ main/js/cookie-consent.js
@@ -9,7 +9,8 @@ document.cookie = "DataCookie=cookie.DataBase_Savedata_thecoolscience; max-age="
 if(document.cookie){ //if cookie is set
 cookieBox.classList.add("hide"); //hide cookie box
 }else{ //if cookie not set then alert an error
-alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+//alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+cookieBox.classList.add("hide"); 
 }
 }
 let checkCookie = document.cookie.indexOf("DataCookie=cookie.DataBase_Savedata_thecoolscience"); //checking our cookie
